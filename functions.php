98de12d58c94c5de94e14a211bb46c30338b093e
ferdin <ferdnorbert@gmail.com>
2026-03-18 16:36:55 -0400

    add fonts and update README.md

--- functions.php
+++ functions.php
@@ -16,3 +16,10 @@ function norbert_academy_theme_setup() {
 }
 
 add_action( 'after_setup_theme', 'norbert_academy_theme_setup' );
+
+function norbert_academy_theme_enqueue_styles() {        
+    // Custom fonts
+    wp_enqueue_style( 'norbert_academy_theme_font_css', get_template_directory_uri() . '/fonts/fonts.css', array(), '2.0' );
+}
+
+add_action( 'wp_enqueue_scripts', 'norbert_academy_theme_enqueue_styles' );
